d978ead6319f28ae3aa510438d2df77d3fa1b698
SALVADOR ESTRAN <s.estran@alumnos.upm.es>
2022-04-13 13:59:53 +0200

    Add files via upload

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -2,10 +2,6 @@
 
 var citas = [];
 
-var pdfs = document.getElementById("pdfs");
-
-var visorpdfs = document.getElementsByTagName("iframe");
-
 var mt = ["Mateo ", 25, 23, 17, 25, 48, 34, 29, 34, 38, 42, 30, 50, 58, 36, 39, 28, 27, 35, 30, 34, 46, 46, 39, 51, 46, 75, 66, 20];
 
 var mc = ["Marcos ", 45, 28, 35, 41, 43, 56, 37, 38, 50, 52, 33, 44, 37, 72, 47, 20];
@@ -13,6 +9,10 @@ var mc = ["Marcos ", 45, 28, 35, 41, 43, 56, 37, 38, 50, 52, 33, 44, 37, 72, 47,
 var lc = ["Lucas ", 80, 52, 38, 44, 39, 49, 50, 56, 62, 42, 54, 59, 35, 35, 32, 31, 37, 43, 48, 47, 38, 71, 56, 53];
 
 var jn = ["Juan ", 51, 25, 36, 54, 47, 71, 53, 59, 41, 42, 57, 50, 38, 31, 27, 33, 26, 40, 42, 31, 25];
+
+var pdfs = document.getElementById("pdfs");
+
+var visorpdfs = document.getElementsByTagName("iframe");
 
 function GeneraCitas() {
   for (var i=1; i<mt.length; i++) {
@@ -39,7 +39,6 @@ function GeneraCitas() {
 
 function GeneraNumeros() {
   pdfs.style.display = "none";
-  visorpdfs[0].setAttribute("src", "");
   if (!miVar) {
     miVar = setInterval(NuevoNumero, 40);
   }  
